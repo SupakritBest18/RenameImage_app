b9c4cb6fca6750ab8febed61119afbe68b104b9d
unknown <supakrit.best2543@gmail.com>
2025-04-01 12:14:48 +0700

    update web app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, send_file
 import os
 import pandas as pd
+import re
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 BASE_URL = "https://assets.central.co.th/{}?$JPG$"
+pattern_remove_cds = r'^CDS' # pattern Remove CDS
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -35,19 +37,25 @@ def upload_file():
 
             if filename in rename_map:  # Check if filename exists in reference
                 new_filename = rename_map[filename]
+                new_filename = re.sub(pattern_remove_cds, "", new_filename)  # Remove 'CDS'
+                file_name_without_ext, _ = os.path.splitext(new_filename)
+
                 new_file_path = os.path.join(UPLOAD_FOLDER, new_filename)
                 
                 file.save(new_file_path)  # Save with new name
                 
+                
+
                 # Generate image URL
                 image_url = BASE_URL.format(new_filename)
-                image_urls.append([new_filename, image_url])
+                #image_urls.append([new_filename, image_url])
+                image_urls.append([file_name_without_ext,image_url])
             else:
                 file.save(os.path.join(UPLOAD_FOLDER, filename))  # Save unchanged
                 image_urls.append([filename, "No new name found"])
 
         # Save output Excel file
-        output_file = os.path.join(UPLOAD_FOLDER, "output_urls.xlsx")
+        output_file = os.path.join(UPLOAD_FOLDER, "output_urls_Rename_DAM.xlsx")
         df_output = pd.DataFrame(image_urls, columns=["Filename", "Image URL"])
         df_output.to_excel(output_file, index=False)
 
